bb56db66f7ffb49d7bf7949b97840548b3a99ab5
Ansh-info <anshkumar.info@gmail.com>
2025-02-03 21:40:03 +0100

    Add: sys path

--- tests/test_state_management.py
+++ tests/test_state_management.py
@@ -1,5 +1,8 @@
 import asyncio
+import os
+import sys
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
 from agent.workflow_graph import WorkflowGraph
 from state.agent_state import AgentStatus
 
