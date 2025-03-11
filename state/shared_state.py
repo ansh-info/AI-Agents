b5dd1f40aaf567397f7de41d361186a8c4628500
Ansh-info <anshkumar.info@gmail.com>
2025-03-11 20:45:38 +0100

    Fix: Used TypedDict

--- state/shared_state.py
+++ state/shared_state.py
@@ -5,12 +5,11 @@ This is the state file for the Talk2Papers agent.
 from typing import Annotated
 import operator
 from langgraph.prebuilt.chat_agent_executor import AgentState
+from typing_extensions import TypedDict
 
 
-class Talk2Papers(AgentState):
+class Talk2Papers(TypedDict, AgentState):
     """The state for the Talk2Papers agent."""
 
-    papers: Annotated[
-        list, operator.add
-    ]  # Using list instead of List for concurrent updates
+    papers: Annotated[list, operator.add]
     search_table: str
